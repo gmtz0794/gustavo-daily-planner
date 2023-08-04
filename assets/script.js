cf03c0e6e3b333e8e101293a4a8f833ab04a6d83
Gustavo Martinez <gmtz.0794@gmail.com>
2023-08-03 17:04:58 -0700

    added js for past, present, future

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -14,7 +14,20 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    var currentHour = dayjs().format('H');
+    var timeBlocks = $('.time-block');
+    timeBlocks.each(function() {
+        var blockHour = parseInt($(this).attr('id').split('-')[1]);
+        if (blockHour < currentHour) {
+            $(this).addClass('past').removeClass('present future');
+        } else if (blockHour === currentHour) {
+            $(this).addClass('present').removeClass('past future');
+        } else {
+            $(this).addClass('future').removeClass('past present');
+        }
+    }
+    )
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
